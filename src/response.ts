9f243d8f66aa36c3895706194478a9359110ccfb
Hector Barazorda <ph.barazorda@gmail.com>
2024-02-17 14:14:36 -0500

    feat: :art: code bot whatsaap

---
+++ src/response.ts
@@ -0,0 +1,25 @@
+export const onePiece = [
+  [
+    {
+      body: "One Piece",
+      media:
+        "https://geekstoreperu.com/cdn/shop/products/20220926_205621_0000_1024x.jpg?v=1664243832",
+    },
+
+    {
+      body: "One Piece",
+      media:
+        "https://geekstoreperu.com/cdn/shop/products/20220926_205621_0000_1024x.jpg?v=1664243832",
+    },
+    {
+      body: "One Piece",
+      media:
+        "https://geekstoreperu.com/cdn/shop/products/20220926_205621_0000_1024x.jpg?v=1664243832",
+    },
+    {
+      body: "One Piece",
+      media:
+        "https://geekstoreperu.com/cdn/shop/products/20220926_205621_0000_1024x.jpg?v=1664243832",
+    },
+  ],
+];
